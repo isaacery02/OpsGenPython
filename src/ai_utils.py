3bebc96495a5cc75eb62b2cba8463a7876898e9c
isaacery <isaac.rayner@gmail.com>
2025-05-13 09:57:10 +0100

    Fixed colour coding

--- src/ai_utils.py
+++ src/ai_utils.py
@@ -5,15 +5,15 @@ import google.generativeai as genai
 
 # --- Terminal Colors ---
 class TerminalColors:
-    HEADER = '\\033[95m'
-    OKBLUE = '\\033[94m'
-    OKCYAN = '\\033[96m'
-    OKGREEN = '\\033[92m'
-    WARNING = '\\033[93m'
-    FAIL = '\\033[91m'
-    ENDC = '\\033[0m'
-    BOLD = '\\033[1m'
-    UNDERLINE = '\\033[4m'
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def format_resources_for_ai(resources, fields_to_extract):
     """Formats a list of resource objects into a string for the AI prompt."""
--- src/config_loader.py
+++ src/config_loader.py
@@ -6,15 +6,15 @@ import datetime
 
 # --- Terminal Colors ---
 class TerminalColors:
-    HEADER = '\\033[95m'
-    OKBLUE = '\\033[94m'
-    OKCYAN = '\\033[96m'
-    OKGREEN = '\\033[92m'
-    WARNING = '\\033[93m'
-    FAIL = '\\033[91m'
-    ENDC = '\\033[0m'
-    BOLD = '\\033[1m'
-    UNDERLINE = '\\033[4m'
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 # --- Configuration File Names ---
 # These might be passed as arguments in a future iteration, but define here for now
